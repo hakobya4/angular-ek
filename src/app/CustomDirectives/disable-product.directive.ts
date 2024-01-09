56201bf80e2a7320c8989c95353cf40aed5aa0c7
Narek Hakobyan <108638724+hakobya4@users.noreply.github.com>
2024-01-09 14:09:39 -0500

    css

--- src/app/CustomDirectives/disable-product.directive.ts
+++ src/app/CustomDirectives/disable-product.directive.ts
@@ -7,5 +7,9 @@ export class DisableProductDirective {
 
   constructor( private element: ElementRef, private renderer: Renderer2) { }
 
-  @Input() disableProduct: boolean
+  @Input() set disableProduct(disable: Boolean){
+    if(disable){
+      this.renderer.addClass(this.element.nativeElement, 'disable-out-of-stock-product' )
+    }
+  }
 }
