ff2e7dd0ee7084eaddcdbe4115181ccdb6dc40e4
Narek Hakobyan <108638724+hakobya4@users.noreply.github.com>
2024-04-21 02:16:03 -0400

    added git ignore and merged the working branch

---
+++ src/app/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  googleClientId:
+    "414696942433-abqv4vfbhfkr1fuqhiohf606c344h0q1.apps.googleusercontent.com",
+  repliersAPI: "V7yY5iJO3aLLIH9bZg1MKHD2hj6d1p",
+};
--- src/app/header/login-form/login-form.component.ts
+++ src/app/header/login-form/login-form.component.ts
@@ -1,21 +1,21 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
 
 // You'll use this import to close the dialog on success
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from "@angular/material/dialog";
 // This import brings in the API calls
 
 // This import is used to display notifications back to the user
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { RegisterFormComponent } from '../register-form/register-form.component';
-import { environment } from '../../environment';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { RegisterFormComponent } from "../register-form/register-form.component";
+import { environment } from "../../environment";
 
 declare var google: any;
 @Component({
-  selector: 'login-form',
-  templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css',
+  selector: "login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrl: "./login-form.component.css",
 })
 export class LoginFormComponent implements OnInit {
   loggedInUser: any;
@@ -26,13 +26,13 @@ export class LoginFormComponent implements OnInit {
         this.handleGoogleLogin(resp);
       },
     });
-    google.accounts.id.renderButton(document.getElementById('google-btn'), {
-      theme: 'filled_blue',
-      size: 'large',
-      shape: 'rectangle',
+    google.accounts.id.renderButton(document.getElementById("google-btn"), {
+      theme: "filled_blue",
+      size: "large",
+      shape: "rectangle",
     });
   }
-  @Input() userData = { Username: '', Password: '' };
+  @Input() userData = { Username: "", Password: "" };
 
   constructor(
     public dialogRef: MatDialogRef<LoginFormComponent>,
@@ -44,20 +44,20 @@ export class LoginFormComponent implements OnInit {
   openRegisterDialog() {
     this.dialogRef.close();
     this.dialog.open(RegisterFormComponent, {
-      width: '50%',
-      height: '70%',
+      width: "50%",
+      height: "70%",
     });
   }
   closeDialogButton() {
     this.dialogRef.close();
   }
   private decodeToken(token: string) {
-    return JSON.parse(atob(token.split('.')[1]));
+    return JSON.parse(atob(token.split(".")[1]));
   }
   handleGoogleLogin(user: any) {
     if (user) {
       const payLoad = this.decodeToken(user.credential);
-      sessionStorage.setItem('loggedInUser', JSON.stringify(payLoad));
+      sessionStorage.setItem("loggedInUser", JSON.stringify(payLoad));
       this.closeDialogButton();
     }
   }
